85ecf487db0d9345def78c2ab9eeb470834394fa
simthelim <44697178+simthelim@users.noreply.github.com>
2018-11-05 14:02:41 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const host = 'api.thingspeak.com';
 
 var unit;
 var state;
-
+var cmd;
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -21,24 +21,35 @@ restService.use(bodyParser.json());
 
 restService.post("/webhook", function(req, res) {
 
- 	unit = req.body.queryResult.parameters['unit']; // take out the unit, lamp e.g.
- 	state = req.body.queryResult.parameters['state']; // take out the the state, on or off
- 
- if (unit == 'light' && state == 'on'){
-	 callThingApiON().then((output) => {
-    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
-  }).catch(() => {
-    res.json({ 'fulfillmentText': 'something is wrong' });
-  });
- }else{
-	callThingApiOFF().then((output) => {
-    res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
-  }).catch(() => {
-    res.json({ 'fulfillmentText': 'something is wrong' });
-  }); 
- }
-});
-
+ 	unit 	= req.body.queryResult.parameters['unit']; 	// take out the unit, ligh e.g.
+ 	state 	= req.body.queryResult.parameters['state']; // retrieve the state of the light.
+ 	cmd		= req.body.queryResult.parameters['cmd']	// retrieve the wanted command intent from Dialogflow.
+ 	if (cmd == 'state') {
+ 		getStateOfLight().then((output) => {
+ 			res.json({'fulfillmentText': 'The light is currently turned' + output});	// Return the results of the weather API to Dialogflow
+			}).catch(() => {
+    		res.json({ 'fulfillmentText': 'something is wrong' });
+  			});
+ 	};
+ };
+//  if (cmd == turn) {
+//  if (unit == 'light' && state == 'on'){
+// 	 callThingApiON().then((output) => {
+//     res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+//   }).catch(() => {
+//     res.json({ 'fulfillmentText': 'something is wrong' });
+//   });
+//  }
+//  else
+//  	{
+// 	callThingApiOFF().then((output) => {
+//     res.json({ 'fulfillmentText': output }); // Return the results of the weather API to Dialogflow
+//   	}).catch(() => {
+//     res.json({ 'fulfillmentText': 'something is wrong' });
+//   	}); 
+//  	}
+// });
+}
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
@@ -46,7 +57,7 @@ restService.listen(process.env.PORT || 8000, function() {
 
 
 
-function callThingApiON () {
+function LightON () {
     return new Promise((resolve, reject) => {
     // Create the path for the HTTP request to get the weather
     //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
@@ -60,7 +71,7 @@ function callThingApiON () {
         //let response = JSON.parse(body);
         //let last = response['field1'];
         // Create response
-        let output = 'Turned on the lamp';
+        let output = 'The light is now turned on';
 
         // Resolve the promise with the output text
         console.log(output);
@@ -88,7 +99,35 @@ function callThingApiOFF () {
         //let response = JSON.parse(body);
         //let last = response['field1'];
         // Create response
-        let output = 'Turned off the lamp';
+        let output = 'The light is now turned off';
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
+
+function getStateOfLight () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+	
+    https.get('https://api.thingspeak.com/channels/619204/feeds.json?results=2', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let temp = response.feeds[0].field1;
+        // Create response
+        let output = state;
 
         // Resolve the promise with the output text
         console.log(output);
