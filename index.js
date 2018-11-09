a371532cef90e11541691856633e59d51043ad65
simthelim <44697178+simthelim@users.noreply.github.com>
2018-11-09 11:27:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -75,50 +75,50 @@ restService.post("/webhook", function(req, res) {
 
 
   //-----------------------------Kitchen Light Control-----------------------------//
-  if (area == 'kitchen') {
-	  //Status of lights
-	  if (cmd == 'state' && unit == 'light') {
-	      getStateOfLight().then((output) => {
-	        if (output == 0) {
-	         res.json({ 'fulfillmentText': 'The light is turned off' }); // Return the state of light
-	        }
-	        else {
-	          res.json({ 'fulfillmentText': 'The light is turned on' }); // Return the state of the light
-	        };
+ //  if (area == 'kitchen') {
+	//   //Status of lights
+	//   if (cmd == 'state' && unit == 'light') {
+	//       getStateOfKitchenLight().then((output) => {
+	//         if (output == 0) {
+	//          res.json({ 'fulfillmentText': 'The kitchen light is turned off' }); // Return the state of light
+	//         }
+	//         else {
+	//           res.json({ 'fulfillmentText': 'The kitchen light is turned on' }); // Return the state of the light
+	//         };
 	          
-	      }).catch(() => {
-	        res.json({ 'fulfillmentText': 'something is wrong' });
-	      });
-	  }; 
+	//       }).catch(() => {
+	//         res.json({ 'fulfillmentText': 'something is wrong' });
+	//       });
+	//   }; 
 
-	  //Switch on/off lights
-	  if (cmd == 'turn' && unit == 'light') {
+	//   //Switch on/off lights
+	//   if (cmd == 'turn' && unit == 'light') {
 	    
-	    if (state == 'on') {
-	      getStateOfLight().then((output) => {                             //Checks the output of getStateOfLight to see if it is already on
-	        if (output == 1) {
-	         res.json({ 'fulfillmentText': 'The lights are already on' }); // If the lights are already on
-	        } else {
-	          turnLightON().then((output) => {                              //Else it turns on the lights.
-	            res.json({ 'fulfillmentText': output });
-	          });
-	        };
-	      });
-	    };
-	    if (state == 'off') {
-	      getStateOfLight().then((output) => {                              //Checks the output of getStateOfLight to see if it is already off
-	        if (output == 0) {
-	         res.json({ 'fulfillmentText': 'The lights are already off' }); // If the lights are already off
-	        } else {
-	          turnLightOFF().then((output) => {                             //Else it turns off the lights
-	            res.json({ 'fulfillmentText': output });
-	          });
-	        };
-	      });
-	    };
+	//     if (state == 'on') {
+	//       getStateOfKitchenLight().then((output) => {                             //Checks the output of getStateOfLight to see if it is already on
+	//         if (output == 1) {
+	//          res.json({ 'fulfillmentText': 'The kitchen lights are already on' }); // If the lights are already on
+	//         } else {
+	//           turnKitchenLightON().then((output) => {                              //Else it turns on the lights.
+	//             res.json({ 'fulfillmentText': output });
+	//           });
+	//         };
+	//       });
+	//     };
+	//     if (state == 'off') {
+	//       getStateOfKitchenLight().then((output) => {                              //Checks the output of getStateOfLight to see if it is already off
+	//         if (output == 0) {
+	//          res.json({ 'fulfillmentText': 'The lights are already off' }); // If the lights are already off
+	//         } else {
+	//           turnKitchenLightOFF().then((output) => {                             //Else it turns off the lights
+	//             res.json({ 'fulfillmentText': output });
+	//           });
+	//         };
+	//       });
+	//     };
 
-	  	};
-	};
+	//   	};
+	// };
 
 
 
@@ -147,7 +147,7 @@ function turnLivingLightON () {
         //let response = JSON.parse(body);
         //let last = response['field1'];
         // Create response
-        let output = 'The light is now turned on';
+        let output = 'The living room light is now turned on';
 
         // Resolve the promise with the output text
         console.log(output);
@@ -218,86 +218,86 @@ function getStateOLivingfLight () {
 }
 
 //----------------------------Kitchen Light------------------------------//
-function turnKitchenLightON () {
-    return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
-    // Make the HTTP request
+// function turnKitchenLightON () {
+//     return new Promise((resolve, reject) => {
+//     // Create the path for the HTTP request to get the weather
+//     //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+//     // Make the HTTP request
   
-    https.get('https://api.thingspeak.com/update?api_key=8GC28PFNII0B3951&field1=1', (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        //let response = JSON.parse(body);
-        //let last = response['field1'];
-        // Create response
-        let output = 'The light is now turned on';
+//     https.get('https://api.thingspeak.com/update?api_key=8GC28PFNII0B3951&field1=1', (res) => {
+//       let body = ''; // var to store the response chunks
+//       res.on('data', (d) => { body += d; }); // store each response chunk
+//       res.on('end', () => {
+//         // After all the data has been received parse the JSON for desired data
+//         //let response = JSON.parse(body);
+//         //let last = response['field1'];
+//         // Create response
+//         let output = 'The light is now turned on';
 
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log('Error calling API')
-        reject();
-      });
-    });
-  });
-}
+//         // Resolve the promise with the output text
+//         console.log(output);
+//         resolve(output);
+//       });
+//       res.on('error', (error) => {
+//         console.log('Error calling API')
+//         reject();
+//       });
+//     });
+//   });
+// }
 
-function turnKitchenLightOFF () {
-    return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=0';
-    // Make the HTTP request
+// function turnKitchenLightOFF () {
+//     return new Promise((resolve, reject) => {
+//     // Create the path for the HTTP request to get the weather
+//     //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=0';
+//     // Make the HTTP request
   
-    https.get('https://api.thingspeak.com/update?api_key=8GC28PFNII0B3951&field1=0', (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        //let response = JSON.parse(body);
-        //let last = response['field1'];
-        // Create response
-        let output = 'The light is now turned off';
+//     https.get('https://api.thingspeak.com/update?api_key=8GC28PFNII0B3951&field1=0', (res) => {
+//       let body = ''; // var to store the response chunks
+//       res.on('data', (d) => { body += d; }); // store each response chunk
+//       res.on('end', () => {
+//         // After all the data has been received parse the JSON for desired data
+//         //let response = JSON.parse(body);
+//         //let last = response['field1'];
+//         // Create response
+//         let output = 'The light is now turned off';
 
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log('Error calling API')
-        reject();
-      });
-    });
-  });
-}
+//         // Resolve the promise with the output text
+//         console.log(output);
+//         resolve(output);
+//       });
+//       res.on('error', (error) => {
+//         console.log('Error calling API')
+//         reject();
+//       });
+//     });
+//   });
+// }
 
-function getStateOfKitchenLight () {
-    return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
-    // Make the HTTP request
+// function getStateOfKitchenLight () {
+//     return new Promise((resolve, reject) => {
+//     // Create the path for the HTTP request to get the weather
+//     //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+//     // Make the HTTP request
   
-    https.get('https://api.thingspeak.com/channels/619204/feeds.json?results=1', (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let temp = response.feeds[0].field1;
-        // Create response
-        let output = temp;
+//     https.get('https://api.thingspeak.com/channels/619204/feeds.json?results=1', (res) => {
+//       let body = ''; // var to store the response chunks
+//       res.on('data', (d) => { body += d; }); // store each response chunk
+//       res.on('end', () => {
+//         // After all the data has been received parse the JSON for desired data
+//         let response = JSON.parse(body);
+//         let temp = response.feeds[0].field1;
+//         // Create response
+//         let output = temp;
 
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        console.log('Error calling API')
-        reject();
-      });
-    });
-  });
-}
+//         // Resolve the promise with the output text
+//         console.log(output);
+//         resolve(output);
+//       });
+//       res.on('error', (error) => {
+//         console.log('Error calling API')
+//         reject();
+//       });
+//     });
+//   });
+// }
