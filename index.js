07bdeb1a92ad4de698834509c0f21f500a75fb25
simthelim <44697178+simthelim@users.noreply.github.com>
2018-11-12 15:22:04 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ var unit;
 var state;
 var cmd;
 var area;
+var percentage;
+
+var volume;
+var vol;
 var controlkey;
 var statekey;
+
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
@@ -27,9 +33,10 @@ restService.post("/webhook", function(req, res) {
   unit  = req.body.queryResult.parameters['unit'];  // take out the unit, light e.g.
   state = req.body.queryResult.parameters['state']; // retrieve the state of the light.
   cmd   = req.body.queryResult.parameters['cmd'];   // retrieve the wanted command intent from Dialogflow.
-  area  = req.body.queryResult.parameters['area'];  // retroeve the wamted area of the device
+  area  = req.body.queryResult.parameters['area'];  // retrieve the wanted area of the device
+  percentage = req.body.queryResult.parameters['percentage']; // retrieve the percentage for the volume
   
-  //Get the correct API-key that relates to the units area
+  //Get the API-key that relates to the correct unit
   if (area == 'living room') {
     controlkey = '8GC28PFNII0B3951';
     statekey = '619204';
@@ -39,8 +46,28 @@ restService.post("/webhook", function(req, res) {
     controlkey = 'NKLUZK54FJP8Q5QP';
     statekey = '622701';
   };
+  // if (unit == 'speaker') {
+  //   controlkey = '';
+  //   statekey = '';
+  // };
+  
+//------------------------------Speaker volume control----------------------------//
+  //Set volume of speaker
+  if (percentage !== null) {
+    volume = percentage.replace( /^\D+/g, ''); // replace all leading non-digits with nothing
+    vol = volume.toString();
+  };
 
-  //-----------------------------Light Control-----------------------------//
+  if (cmd == 'set' && unit == 'speaker') {
+    setSpeakerVolume().then((output) => {
+      res.json({ 'fulfillmentText': output });
+    });
+  };
+
+
+
+
+  //-----------------------------Light Control------------------------------------//
 
   //Status of lights
   if (cmd == 'state' && unit == 'light') {
@@ -95,7 +122,61 @@ restService.listen(process.env.PORT || 8000, function() {
 });
 
 
+function setSpeakerVolume () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+  
+    https.get('https://api.thingspeak.com/update?api_key=8YUPDMQJS9LFKDM2&field1='+vol, (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        //let response = JSON.parse(body);
+        //let last = response['field1'];
+        // Create response
+        let output = 'The speaker is now on '+vol+' percent.';
 
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
+
+function getSpeakerVolume () {
+    return new Promise((resolve, reject) => {
+    // Create the path for the HTTP request to get the weather
+    //let path = '/update?api_key=116UAXMQP1O8EYZ3&field1=1';
+    // Make the HTTP request
+  
+    https.get('https://api.thingspeak.com/channels/624654/feeds.json?results=2', (res) => {
+      let body = ''; // var to store the response chunks
+      res.on('data', (d) => { body += d; }); // store each response chunk
+      res.on('end', () => {
+        // After all the data has been received parse the JSON for desired data
+        let response = JSON.parse(body);
+        let temp = response.feeds[0].field1;
+        // Create response
+        let output = temp;
+
+        // Resolve the promise with the output text
+        console.log(output);
+        resolve(output);
+      });
+      res.on('error', (error) => {
+        console.log('Error calling API')
+        reject();
+      });
+    });
+  });
+}
 
 function turnLightON () {
     return new Promise((resolve, reject) => {
