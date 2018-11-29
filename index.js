be860baaf7b0ed868c49244513a683cc271a61ba
simthelim <44697178+simthelim@users.noreply.github.com>
2018-11-29 10:44:54 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ var state;
 var cmd;
 var area;
 var percentage;
+var regulate;
 
 var volume;
 var vol;
@@ -35,10 +36,11 @@ restService.post("/webhook", function(req, res) {
   unit  = req.body.queryResult.parameters['unit'];  // take out the unit, light e.g.
   state = req.body.queryResult.parameters['state']; // retrieve the state of the light.
   cmd   = req.body.queryResult.parameters['cmd'];   // retrieve the wanted command intent from Dialogflow.
-  area  = req.body.queryResult.parameters['area'];  // retrieve the wanted area of the device
-  percentage = req.body.queryResult.parameters['percentage']; // retrieve the percentage for the volume
+  area  = req.body.queryResult.parameters['area'];  // retrieve the wanted area of the device.
+  percentage = req.body.queryResult.parameters['percentage']; // retrieve the percentage for the fan speed.
+  regulate = req.body.queryResult.parameters['regulate']; // retrieve the regulate parameter.
   
-  //Get the API-key that relates to the correct unit
+  //Get the API-key that relates to the correct unit in the smart home
   if (area == 'living room') {
     controlkey = 'VCR3Z71LGJYQ9MSO';
     statekey = '625581';
@@ -51,16 +53,37 @@ restService.post("/webhook", function(req, res) {
     brokenkey = '625546';
   };
   
-//------------------------------Speaker volume control----------------------------//
-  //Set volume of speaker
-  if (cmd == 'set' && unit == 'speaker') {
-    volume = percentage.replace( /^\D+/g, ''); // Take away the %-sign from 'percentage'
-    setSpeakerVolume().then((output) => {
+//------------------------------Fan control----------------------------//
+if (unit == 'fan') {
+  //Set fan speed
+  if (cmd == 'set') {
+    speed = percentage.replace( /^\D+/g, ''); // Take away the %-sign from 'percentage'
+    setFanSpeed().then((output) => {
       res.json({ 'fulfillmentText': output });
     });
   };
 
+ 	//Increase/Decrease fan speed
+ 	if (regulate == 'increase'){
+ 		spd = percentage.replace( /^\D+/g, ''); // Take away the %-sign from 'percentage'
+ 		getFanSpeed().then((outupt) => {
+ 			speed = spd + output;
+ 			if (speed > 100) {
+ 				speed = 100;
+ 			};
+ 			setFanSpeed().then((fanSpeed) => {
+      	res.json({ 'fulfillmentText': fanSpeed });
+    	});
 
+ 		});
+ 	};
+
+
+ 	if (regulate == 'decrease'){
+
+ 	};
+
+};
 //-------------------------------Temperature--------------------------------------//
 if(cmd == 'temperature') {
 	getTemperature().then((temp) => {
@@ -72,19 +95,19 @@ if(cmd == 'temperature') {
 
   //Status of lights
   if (cmd == 'state' && unit == 'light') {
-      getStateOfLight().then((output) => {
-        isLightBroken().then((broken) =>{
-        if (output == 0 && broken == 0) {
-          res.json({ 'fulfillmentText': 'The '+area+' light is turned off' }); // Return the state of the light
+      getStateOfLight().then((output) => {					//Get the state of the light channel.
+        isLightBroken().then((broken) =>{						//Get the state of the actual light channel to see if it is broken.
+        if (output == 0 && broken == 0) {						//If both are 0 the light should be turned off.
+          res.json({ 'fulfillmentText': 'The '+area+' light is turned off' }); //Return the state of the light.
         }
-        else if (output == 1 && broken == 1) {
-          res.json({ 'fulfillmentText': 'The '+area+' light is turned on' }); // Return the state of the light
+        else if (output == 1 && broken == 1) {																//If both are 1 the light should be turned on.
+          res.json({ 'fulfillmentText': 'The '+area+' light is turned on' }); //Return the state of the light.
         }
-        else {
-        	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });
+        else {																																			//Else if the channels do not have the 
+        	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });	//same channel status the light is broken.
         };
       }).catch(() => {
-        res.json({ 'fulfillmentText': 'something is wrong' });
+        res.json({ 'fulfillmentText': 'something is wrong' });											//Catch the reject error from the promise.
       });
     });
   }; 
@@ -92,41 +115,41 @@ if(cmd == 'temperature') {
   //Switch on/off lights
   if (cmd == 'turn' && unit == 'light') {
 
-    if (state == 'on') {
-      getStateOfLight().then((output) => {                             //Checks the output of getStateOfLight to see if it is already on
-        isLightBroken().then((broken) =>{
-        if (output == 1 && broken == 1) {
-         res.json({ 'fulfillmentText': 'The '+area+' lights are already on' }); // If the lights are already on
+    if (state == 'on') {														//If state == on we want to turn on the lights.
+      getStateOfLight().then((output) => {          //Get the state of the light channel.
+        isLightBroken().then((broken) =>{						//Get the state of the actual light channel to see if it's broken.
+        if (output == 1 && broken == 1) {						//Both channels == 1, the lights are already on.
+         res.json({ 'fulfillmentText': 'The '+area+' lights are already on' }); //Output if the lights are already on
         }
-        else if(output == 0 && broken == 1){
+        else if(output == 0 && broken == 1){				//If the channel states are different the lights are broken.
         	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });
         }
         else if(output == 1 && broken == 0){
         	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });
         }
         else {
-          turnLightON().then((output) => {                              //Else it turns on the lights.
-            res.json({ 'fulfillmentText': output });
+          turnLightON().then((output) => {            //Else if the channels are not both == 1 or different
+            res.json({ 'fulfillmentText': output });	//they should both be zero and we can therofore turn on the lights.
           });
         };
         });
       });
     };
-    if (state == 'off') {
-      getStateOfLight().then((output) => {                              //Checks the output of getStateOfLight to see if it is already off
-        isLightBroken().then((broken) =>{
-        if (output == 0 && broken == 0) {
-         res.json({ 'fulfillmentText': 'The '+area+' lights are already off' }); // If the lights are already off
+    if (state == 'off') {														//If state == on we want to turn off the lights.
+      getStateOfLight().then((output) => {          //Get the state of the light channel.
+        isLightBroken().then((broken) =>{						//Get the state of the actual light channel to see if it's broken.
+        if (output == 0 && broken == 0) {						//Both channels == 0, the lights are already off.
+         res.json({ 'fulfillmentText': 'The '+area+' lights are already off' }); //Output if the lights are already off.
         }
-        else if(output == 0 && broken == 1){
+        else if(output == 0 && broken == 1){				//If the channel states are different the lights are broken.
         	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });
         }
         else if(output == 1 && broken == 0){
         	res.json({ 'fulfillmentText': 'The '+area+' light seems to be broken' });
         }
          else {
-          turnLightOFF().then((output) => {                             //Else it turns off the lights
-            res.json({ 'fulfillmentText': output });
+          turnLightOFF().then((output) => {           //Else if the channels are not both == 0 or different
+            res.json({ 'fulfillmentText': output });	//they should both be 1 and we can therofore turn off the lights.
           });
         };
       });
@@ -168,16 +191,16 @@ function getTemperature () {
   });
 }
 
-function setSpeakerVolume () {
+function setFanSpeed () {
     return new Promise((resolve, reject) => {
     // Make the HTTP request  
-    https.get('https://api.thingspeak.com/update?api_key=8YUPDMQJS9LFKDM2&field1='+volume, (res) => {
+    https.get('https://api.thingspeak.com/update?api_key=8YUPDMQJS9LFKDM2&field1='+speed, (res) => {
       let body = ''; // var to store the response chunks
       res.on('data', (d) => { body += d; }); // store each response chunk
       res.on('end', () => {
         // After all the data has been received parse the JSON for desired data
         // Create response
-        let output = 'The speaker is now at '+vol+' percent.';
+        let output = 'The fan is now at '+speed+' percent of maximum speed.';
         // Resolve the promise with the output text
         console.log(output);
         resolve(output);
@@ -190,10 +213,10 @@ function setSpeakerVolume () {
   });
 }
 
-function getSpeakerVolume () {
+function getFanSpeed () {
     return new Promise((resolve, reject) => {
     // Make the HTTP request  
-    https.get('https://api.thingspeak.com/channels/624654/feeds.json?results=2', (res) => {
+    https.get('https://api.thingspeak.com/channels/624654/feeds.json?results=1', (res) => {
       let body = ''; // var to store the response chunks
       res.on('data', (d) => { body += d; }); // store each response chunk
       res.on('end', () => {
@@ -214,15 +237,13 @@ function getSpeakerVolume () {
 }
 
 function turnLightON () {
-    return new Promise((resolve, reject) => {
-    // Make the HTTP request  
-    https.get('https://api.thingspeak.com/update?api_key='+controlkey+'&field1=1', (res) => {
+    return new Promise((resolve, reject) => {     
+    https.get('https://api.thingspeak.com/update?api_key='+controlkey+'&field1=1', (res) => {	// Make the HTTP request 
       let body = ''; // var to store the response chunks
       res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // Create response
-        let output;
-        if (body == '0') {
+      res.on('end', () => {        
+        let output;													// Create response
+        if (body == '0') {									//The http request did not change the thingspeak channel
           output = 'The light did not turn on, please try again in a moment.';
         } else {
           output = 'Turning on the '+area+' light.';
@@ -251,7 +272,7 @@ function turnLightOFF () {
         if (body == '0') {
           output = 'The light did not turn off, please try again in a moment.';
         } else {
-          output = 'Turning off the  '+area+'light.';
+          output = 'Turning off the  '+area+' light.';
         };
         // Create response
         // Resolve the promise with the output text
